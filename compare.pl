d8b4ac9dd866abdc35a8fa16a45b70592188288c
jbukhari <joe.bukhari@gmail.com>
2017-06-23 11:03:02 -0400

    made matching case and space insenstive; updated results

--- compare.pl
+++ compare.pl
@@ -4,12 +4,17 @@ use feature 'say';
 use Data::Dumper;
 
 open my $ods,'<',$ARGV[0];
-open my $esc,'<',$ARGV[1];
-my %esc;
-while (<$esc>) {
+open my $dls,'<',$ARGV[1];
+my %dls;
+while (<$dls>) {
 	s/[\r\n]//g;
 	my @row = split "\t", $_;
-	$esc{$_} = $row[0] for split /; /, $row[1];
+	my $id = $row[0];
+	for my $sym (split /; /, $row[1]) {
+		my $match = ds($sym);
+		$dls{$match}{dls_id} = $id;
+		$dls{$match}{og_sym} = $sym;
+	}
 }
 my %ods;
 open my $out1,'>','in_ods.tsv';
@@ -18,19 +23,26 @@ while (<$ods>) {
 	my @row = split ',', $_;
 	my $sym = $row[0];
 	$sym =~ s/"//g;
-	$ods{$sym} = 1;
-	if ($esc{$sym}) {
-		say $out1 join "\t", $esc{$sym}, $sym, 'IN DLS';
+	my $match = ds($sym);
+	$ods{$match} = 1;
+	if ($dls{$match}) {
+		say $out1 join "\t", $dls{$match}{dls_id}, $sym, 'IN DLS';
 	} else {
 		say $out1 join "\t", '', $sym, 'NOT IN DLS';
 	}
 }
 open my $out2,'>','in_dls.tsv';
-for (keys %esc) {
-	if (! $ods{$_}) {
-		say $out2 join "\t", $esc{$_}, $_, 'NOT IN ODS';
+for my $match (keys %dls) {
+	if ($ods{$match}) {
+		say $out2 join "\t", $dls{$match}{dls_id}, $dls{$match}{og_sym}, 'IN ODS';
 	} else {
-		say $out2 join "\t", $esc{$_}, $_, 'IN ODS';
+		say $out2 join "\t", $dls{$match}{dls_id}, $dls{$match}{og_sym}, 'NOT IN ODS';
 	}
 }
 
+sub ds {
+	my $sym = shift;
+	$sym = lc $sym;
+	$sym =~ s/ //g;
+	return $sym;
+}
